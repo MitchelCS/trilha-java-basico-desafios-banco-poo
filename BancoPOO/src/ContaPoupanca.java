5b504322eae99ecd59700180f8afcdb13abb75a3
Mitchel Camara Silva <168555075+MitchelCS@users.noreply.github.com>
2024-07-11 19:12:15 -0300

    Update ContaPoupanca.java

--- BancoPOO/src/ContaPoupanca.java
+++ BancoPOO/src/ContaPoupanca.java
@@ -2,6 +2,3 @@ public class ContaPoupanca extends Conta {
     public ContaPoupanca(String numero) {
         super(numero);
     }
-
-    // Conta Poupança pode ter funcionalidades específicas, adicionadas aqui
-}
